f33ab74dca79ccddeec8eb2a3b15478795402bf7
emanueledb <emanueledibene@gmail.com>
2024-02-09 11:49:56 +0100

    arrow implementation completed

--- src/components/GenreSection.vue
+++ src/components/GenreSection.vue
@@ -1,12 +1,37 @@
 <template>
-  <div class="flex flex-col p-4">
-    <div v-for="genre of genreGroups" :key="genre.genre" class="flex flex-col">
-      <h2 class="mt-3 text-xl">{{ genre.genre }}</h2>
+  <div class="flex flex-col px-4">
+    <div
+      v-for="genre of genreGroups"
+      :key="genre.genre"
+      class="flex flex-col relative group"
+    >
+      <h2 class="mt-3 text-2xl">{{ genre.genre }}</h2>
 
-      <div class="flex overflow-x-auto h-40 lg:h-80">
+      <div
+        :ref="(el) => setGenreRef(genre.genre, el as HTMLDivElement)"
+        class="flex overflow-x-auto h-40 lg:h-80 scroll-smooth"
+      >
         <div v-for="show of sortedShows(genre.TvShows)" :key="show.name">
           <ShowCard :show="show" />
         </div>
+      </div>
+
+      <div
+        class="hidden lg:flex lg:absolute lg:top-0 lg:left-0 lg:h-full lg:w-10 lg:bg-gradient-to-r lg:from-white lg:to-transparent lg:opacity-0 lg:group-hover:opacity-100 lg:items-center lg:justify-center lg:cursor-pointer"
+        @click="scrollLeft(genre.genre)"
+      >
+        <Icon
+          class="transform rotate-180"
+          name="material-symbols:play-arrow-outline-rounded"
+          size="50"
+        />
+      </div>
+
+      <div
+        class="hidden lg:flex lg:absolute lg:top-0 lg:right-0 lg:h-full lg:w-10 lg:bg-gradient-to-l lg:from-white lg:to-transparent lg:opacity-0 lg:group-hover:opacity-100 lg:items-center lg:justify-center lg:cursor-pointer"
+        @click="scrollRight(genre.genre)"
+      >
+        <Icon name="material-symbols:play-arrow-outline-rounded" size="50" />
       </div>
     </div>
   </div>
@@ -15,11 +40,35 @@
 <script setup lang="ts">
 import type { TGroup, TTvShow } from '~/types/types'
 
+interface GenreRefs {
+  [genre: string]: HTMLDivElement | null
+}
+
 defineProps({
   genreGroups: { type: Array<TGroup>, required: true },
 })
 
 const sortedShows = (shows: TTvShow[]) => {
   return [...shows].sort((a, b) => b.rating.average - a.rating.average)
+}
+
+const genreRefs = ref<GenreRefs>({})
+
+const setGenreRef = (genre: string, el: HTMLDivElement | null) => {
+  genreRefs.value[genre] = el
+}
+
+const scrollLeft = (genre: string) => {
+  const el = genreRefs.value[genre]
+  if (el) {
+    el.scrollLeft -= 300
+  }
+}
+
+const scrollRight = (genre: string) => {
+  const el = genreRefs.value[genre]
+  if (el) {
+    el.scrollLeft += 300
+  }
 }
 </script>
