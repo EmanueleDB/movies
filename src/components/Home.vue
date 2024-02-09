1de15e309f1b340362c2d09bfca5860ec1e4d784
emanueledb <emanueledibene@gmail.com>
2024-02-09 20:26:54 +0100

    search feature implemented - needs style

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,7 +2,23 @@
   <div v-if="!data" class="flex justify-center mt-3">
     <Icon class="text-primary" name="svg-spinners:blocks-shuffle-3" size="50" />
   </div>
-  <Genre v-else :genre-groups="genreGroups" />
+  <div v-else class="flex flex-col mt-3">
+    <input
+      v-model="searchQuery"
+      type="search"
+      placeholder="Search for a TV show"
+      @input="handleSearch"
+    />
+    <div v-if="searchQuery" class="ml-3 mt-3 flex flex-wrap h-40 lg:h-80">
+      <TvShow
+        v-for="(item, index) of foundTvShow"
+        :key="index"
+        class=""
+        :show="item.show"
+      />
+    </div>
+    <Genres v-else :genre-groups="genreGroups" />
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -29,5 +45,23 @@ const groupByGenre = (data: Array<TTvShow>) => {
     })
     return acc
   }, [])
+}
+
+let timeoutId: any = null
+const handleSearch = () => {
+  clearTimeout(timeoutId)
+  timeoutId = setTimeout(() => {
+    performSearch()
+  }, 500)
+}
+
+const searchQuery = ref('')
+const foundTvShow = ref({})
+const performSearch = async () => {
+  if (searchQuery.value) {
+    foundTvShow.value = await $fetch(
+      `https://api.tvmaze.com/search/shows?q=${searchQuery.value}`,
+    )
+  }
 }
 </script>
--- src/components/TvShow.vue
+++ src/components/TvShow.vue
@@ -4,74 +4,83 @@
     @click="fetchSeasons(show.id)"
   >
     <img
-      class="h-full object-cover rounded-lg transition-transform duration-200 hover:scale-95 shadow-black shadow-md"
-      :src="show.image.original"
-      :alt="show.image.original"
+      class="h-full object-cover rounded-lg transition-transform duration-200 hover:scale-95 hover:shadow-black hover:shadow-md"
+      :src="show.image ? show.image.original : '/placeholder.jpg'"
+      :alt="show.image ? show.image.original : 'placeholder'"
     />
-  </div>
-  <Slide v-if="showSlide" @close="showSlide = false">
-    <div class="flex">
-      <div class="flex flex-col px-2 lg:px-8">
-        <h2 class="text-2xl">{{ show.name }}</h2>
-        <span>
-          <Icon
-            class="text-primary"
-            name="streamline:interface-favorite-award-ribbon-reward-like-social-rating-media"
-            size="20"
-          />
-          {{ show.rating.average }}</span
-        >
-        <div class="flex mt-3">
-          <img class="w-48 h-60 mt-3" :src="show.image.original" alt="image" />
-          <div class="flex flex-col ml-3">
-            <span class="mt-3">
-              <Icon
-                class="text-primary"
-                name="material-symbols:connected-tv-outline-sharp"
-                size="20"
-              />
-              {{ show.network.name }}</span
-            >
-            <span class="mt-3">
-              <Icon class="text-primary" name="iconoir:language" size="20" />
-              {{ show.language }}</span
-            >
-            <div class="flex flex-wrap w-full mt-3">
-              <Icon
-                class="text-primary"
-                name="streamline:ai-generate-variation-spark"
-                size="20"
-              />
-              <span v-for="genre of show.genres" :key="genre" class="ml-1">{{
-                genre
-              }}</span>
+
+    <Slide v-if="showSlide" @close="showSlide = false">
+      <div class="flex">
+        <div class="flex flex-col px-2 lg:px-8">
+          <h2 class="text-2xl">{{ show.name }}</h2>
+          <span>
+            <Icon
+              class="text-primary"
+              name="streamline:interface-favorite-award-ribbon-reward-like-social-rating-media"
+              size="20"
+            />
+            {{ show.rating.average }}</span
+          >
+          <div class="flex mt-3">
+            <img
+              class="w-48 h-60 mt-3"
+              :src="show.image.original"
+              alt="image"
+            />
+            <div class="flex flex-col ml-3">
+              <span class="mt-3">
+                <Icon
+                  class="text-primary"
+                  name="material-symbols:connected-tv-outline-sharp"
+                  size="20"
+                />
+                {{ show.network.name }}</span
+              >
+              <span class="mt-3">
+                <Icon class="text-primary" name="iconoir:language" size="20" />
+                {{ show.language }}</span
+              >
+              <div class="flex flex-wrap w-full mt-3">
+                <Icon
+                  class="text-primary"
+                  name="streamline:ai-generate-variation-spark"
+                  size="20"
+                />
+                <span v-for="genre of show.genres" :key="genre" class="ml-1">{{
+                  genre
+                }}</span>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-    <div v-if="seasons[0].image.length" class="flex flex-col mt-3 px-2 lg:px-8">
-      <h3 class="text-xl">Seasons</h3>
-      <div class="flex flex-wrap mt-3 w-full">
-        <img
-          v-for="season of seasons"
-          :key="season.id"
-          class="w-[calc(25%-12px)] md:w-24 h-full mr-3 mb-3"
-          :src="season.image ? season.image : ''"
-          alt="season-image"
-        />
+      <div
+        v-if="seasons[0].image.length"
+        class="flex flex-col mt-3 px-2 lg:px-8"
+      >
+        <h3 class="text-xl">Seasons</h3>
+        <div class="flex flex-wrap mt-3 w-full">
+          <img
+            v-for="season of seasons"
+            :key="season.id"
+            class="w-[calc(25%-12px)] md:w-24 h-full mr-3 mb-3"
+            :src="season.image ? season.image : ''"
+            alt="season-image"
+          />
+        </div>
       </div>
-    </div>
-    <div class="flex mt-3 px-2 lg:px-8">
-      <span v-html="show.summary" />
-    </div>
-  </Slide>
+      <div class="flex mt-3 px-2 lg:px-8">
+        <span v-html="show.summary" />
+      </div>
+    </Slide>
+  </div>
 </template>
 
 <script setup lang="ts">
 import type { PropType } from 'vue'
 import type { TTvShow } from '~/types/types'
 import Slide from '~/components/Slide.vue'
+import { placeholder } from '@babel/types'
 
 interface ISeason {
   id: number
