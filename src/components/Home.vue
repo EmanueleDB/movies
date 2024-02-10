cdd9886829a463d82e311c6642c780476e4aa241
Emanuele Di Bene <emanueledibene@gmail.com>
2024-02-10 16:05:46 +0100

    search component separated - styled

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,14 +2,9 @@
   <div v-if="!data" class="flex justify-center mt-3">
     <Icon class="text-primary" name="svg-spinners:blocks-shuffle-3" size="50" />
   </div>
-  <div v-else class="flex flex-col mt-3">
-    <input
-      v-model="searchQuery"
-      type="search"
-      placeholder="Search for a TV show"
-      @input="handleSearch"
-    />
-    <div v-if="searchQuery" class="ml-3 mt-3 flex flex-wrap h-40 lg:h-80">
+  <div v-else class="flex flex-col mt-4 lg:mt-8 px-4 lg:px-10">
+    <Search v-model="searchQuery" />
+    <div v-if="searchQuery" class="mt-3 flex flex-wrap h-40 lg:h-80">
       <TvShow
         v-for="(item, index) of foundTvShow"
         :key="index"
@@ -30,7 +25,7 @@ const { data } = await useFetch<Array<TTvShow>>(
 )
 
 const genreGroups = computed(() => {
-  if (data.value) return groupByGenre(data.value)
+  if (data.value) return groupByGenre(data.value as Array<TTvShow>)
 })
 
 const groupByGenre = (data: Array<TTvShow>) => {
@@ -47,6 +42,11 @@ const groupByGenre = (data: Array<TTvShow>) => {
   }, [])
 }
 
+const searchQuery = ref('')
+watch(searchQuery, (to) => {
+  if (to.length) handleSearch()
+})
+
 let timeoutId: any = null
 const handleSearch = () => {
   clearTimeout(timeoutId)
@@ -55,9 +55,9 @@ const handleSearch = () => {
   }, 500)
 }
 
-const searchQuery = ref('')
 const foundTvShow = ref({})
 const performSearch = async () => {
+  foundTvShow.value = []
   if (searchQuery.value) {
     foundTvShow.value = await $fetch(
       `https://api.tvmaze.com/search/shows?q=${searchQuery.value}`,
---
+++ src/components/Search.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="relative w-full lg:w-2/4">
+    <input
+      v-model="model"
+      type="search"
+      placeholder="Search for a TV show"
+      class="w-full pl-7 py-2 rounded outline-0 border-primary border-2 appearance-none"
+      @input="handleSearch"
+    />
+    <Icon
+      v-if="model"
+      name="ic:baseline-clear"
+      size="20"
+      class="text-primary absolute right-2 top-3"
+      @click="model = ''"
+    />
+    <Icon name="material-symbols:search-rounded" size="20" class="text-primary absolute left-2 top-3" />
+  </div>
+</template>
+
+<script setup lang="ts">
+const model = defineModel({ type: String, default: '' })
+</script>
+
+<style scoped>
+input[type='search']::-webkit-search-decoration,
+input[type='search']::-webkit-search-cancel-button,
+input[type='search']::-webkit-search-results-button,
+input[type='search']::-webkit-search-results-decoration {
+  display: none;
+}
+</style>
